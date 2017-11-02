0f7d49ea891ee6a0c5666f1e0aaa810a3b048c68
Grant Berryman <grantberryman@gmail.com>
2017-11-02 12:59:42 -0500

    person and product class and controllers created

---
+++ ASPNETKata/Controllers/PersonController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPNETKata.Models;
+
+namespace ASPNETKata.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            var list = new List<Person>();
+
+            list.Add(new Person { Age = 14, IsMinor = true, Name = "Harry" });
+            list.Add(new Person { Age = 16, IsMinor = true, Name = "John" });
+            list.Add(new Person { Age = 19, IsMinor = false, Name = "Billy" });
+            list.Add(new Person { Age = 60, IsMinor = false, Name = "Jerry" });
+            list.Add(new Person { Age = 25, IsMinor = false, Name = "Bob" });
+            list.Add(new Person { Age = 17, IsMinor = true, Name = "Ted" });
+
+            return View();
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
