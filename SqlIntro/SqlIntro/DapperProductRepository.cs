a2aa42c515d8ab01d5a08a838f20a502d1bc20b0
Grant Berryman <grantberryman@gmail.com>
2017-11-14 13:13:08 -0600

    fixed all comments from review

--- SqlIntro/SqlIntro/DapperProductRepository.cs
+++ SqlIntro/SqlIntro/DapperProductRepository.cs
@@ -6,41 +6,41 @@ using Dapper;
 
 namespace SqlIntro
 {
-    public class  DapperProductRepository: IProductRepository
+    public class DapperProductRepository : IProductRepository
     {
         private readonly IDbConnection conn;
 
         public DapperProductRepository(IDbConnection conn)
         {
-           this.conn = conn;
+            this.conn = conn;
         }
         public IEnumerable<Product> GetProducts()
         {
             return conn.Query<Product>("Select * from Product");
         }
         public Product GetDetails(int productId)
-        {      
-             return conn.Query<Product>("Select * from Product Where ProductID = @id", new {id = productId }).FirstOrDefault();        
+        {
+            return conn.Query<Product>("Select * from Product Where ProductID = @id", new { id = productId }).FirstOrDefault();
         }
         public void DeleteProduct(int productId)
         {
-                conn.Execute("Delete from Product Where ProductId = @id", new { id = productId });
+            conn.Execute("Delete from Product Where ProductId = @id", new { id = productId });
         }
         public void UpdateProduct(Product prod)
-        { 
-                conn.Execute("Update Product Set Name = @Name Where ProductId = @id", new { id = prod.ProductId, name = prod.Name});
+        {
+            conn.Execute("Update Product Set Name = @Name Where ProductId = @id", new { id = prod.ProductId, name = prod.Name });
         }
         public void InsertProduct(Product prod)
         {
-                conn.Execute("Insert into Product (Name) values (@Name)", new {name = prod.Name});
+            conn.Execute("Insert into Product (Name) values (@Name)", new { name = prod.Name });
         }
-        public IEnumerable<Product> LeftJoinComments()
+        public IEnumerable<Product> FindAllProducts()
         {
-               return conn.Query<Product>("Select*From Product p left join productreview pr on p.ProductId = pr.ProductId");
+            return conn.Query<Product>("Select * From Product p left join productreview pr on p.ProductId = pr.ProductId");
         }
-        public IEnumerable<Product> InnerJoinComments()
+        public IEnumerable<Product> FindAllProdcutsWithReviews()
         {
-                return conn.Query<Product>("Select*From Product p inner join productreview pr on p.ProductId = pr.ProductId");
+            return conn.Query<Product>("Select*From Product p inner join productreview pr on p.ProductId = pr.ProductId");
         }
     }
 }
